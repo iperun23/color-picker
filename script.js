25f057057c2de72a58a0077127253df42fd65b87
Igor Perun <iperun23@gmail.com>
2018-04-10 22:31:17 -0400

    added random button js

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("#colorbutton");
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     css.textContent = body.style.background + ";";
 }
 
+// get random number:
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  // set random number:
+function setGradientSeed() {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    + ", " 
+    + color2.value
+    + ")";
+   
+    css.textContent = body.style.background + ";";
+    }
 
 
 window.addEventListener("load", setGradient);
@@ -15,3 +38,5 @@ window.addEventListener("load", setGradient);
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener ("click", setGradientSeed);
