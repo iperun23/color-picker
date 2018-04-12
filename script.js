9155c007bab19c153bc85a66b2a892ab79c8de6f
Igor Perun <iperun23@gmail.com>
2018-04-12 10:05:00 -0400

    JavaScript syntax changes to es6

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var button = document.querySelector("#colorbutton");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const button = document.querySelector("#colorbutton");
 
-function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+const setGradient = () => {
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value + ", " 
+    + color2.value + ")";
     css.textContent = body.style.background + ";";
-}
+};
 
-// get random number:
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+// Get random number:
+
+const getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  // set random number:
-function setGradientSeed() {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    };
+
+  // Set random number:
+
+const setGradientSeed = () => {
     color1.value = getRandomColor();
     color2.value = getRandomColor();
-    body.style.background = 
-    "linear-gradient(to right, "
-    + color1.value
-    + ", " 
-    + color2.value
-    + ")";
-   
-    css.textContent = body.style.background + ";";
-    }
-
+    setGradient();
+  };
 
 window.addEventListener("load", setGradient);
 
